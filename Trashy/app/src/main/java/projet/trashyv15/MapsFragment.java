2a6d501cdc943242fd91ead338e89ded63fac88e
ph553 <zbentoura@hotmail.com>
2017-04-26 02:30:51 -0400

    debut de detection auto

--- Trashy/app/src/main/java/projet/trashyv15/MapsFragment.java
+++ Trashy/app/src/main/java/projet/trashyv15/MapsFragment.java
@@ -88,7 +88,16 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
         accedCarte.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+
+                if(containsLocation(loc,ahunCV1.m)){
+
+                }
+                else if(containsLocation(loc,vm1.m)||containsLocation(loc,vm2.m)){
+                    Toast.makeText(getContext(), "ville marie", Toast.LENGTH_LONG).show();
+                }
+
+
+
             }
         });
 
@@ -451,6 +460,40 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
 
 
     }
+
+    private boolean containsLocation(LatLng loc, LatLng[] vertices) {
+        int intersectCount = 0;
+        for (int j = 0; j < vertices.length - 1; j++) {
+            if (rayCastIntersect(loc, vertices[j], vertices[j+1])) {
+                intersectCount++;
+            }
+        }
+
+        return ((intersectCount % 2) == 1); // odd = inside, even = outside;
+    }
+
+    private boolean rayCastIntersect(LatLng tap, LatLng vertA, LatLng vertB) {
+
+        double aY = vertA.latitude;
+        double bY = vertB.latitude;
+        double aX = vertA.longitude;
+        double bX = vertB.longitude;
+        double pY = tap.latitude;
+        double pX = tap.longitude;
+
+        if ((aY > pY && bY > pY) || (aY < pY && bY < pY)
+                || (aX < pX && bX < pX)) {
+            return false; // a and b can't both be above or below pt.y, and a or
+            // b must be east of pt.x
+        }
+
+        double m = (aY - bY) / (aX - bX); // Rise over run
+        double bee = (-aX) * m + aY; // y = mx + b
+        double x = (pY - bee) / m; // algebra is neat!
+
+        return x > pX;
+    }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
